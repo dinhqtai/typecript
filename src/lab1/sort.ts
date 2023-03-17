ac095edfc84e89fcea7cd79ad2c554d7be1ef428
dinhqtai <dinhquoctai2003@gmail.com>
2023-03-17 14:41:28 +0700

    main

--- src/lab1/sort.ts
+++ src/lab1/sort.ts
@@ -1,18 +1,15 @@
 const arrSort1 = [2,9,10,6,10];
-function labSort<T> (arr1:T[],callback?:(a:T,b:T)=>T):T[]{
-let temp1 = [];
+function labSort (arr1:number[],callback?:(a:number,b:number)=>number){
 for(let i = 0;i<arr1.length-1;i++){
 for(let j = i+1;j<arr1.length;i++){
-    if(arr1[i]<arr1[j]){
+    const newItem = callback(arr1[i],arr1[j]);
+    if(newItem > 0){
         let temp = arr1[i]
         arr1[i] = arr1[j]
         arr1[j] = temp
     }
-    const newItem = callback(arr1[i],arr1[j]);
-    temp1.push(newItem);
 }
     }
-    return temp1;
     
 }
 const taingu = labSort(arrSort1,(a,b)=>{
