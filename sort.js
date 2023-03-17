e3b9935979a63bd2adb6379b8b44be69372d716c
dinhqtai <dinhquoctai2003@gmail.com>
2023-03-17 15:49:42 +0700

    main

--- sort.js
+++ sort.js
@@ -1,6 +1,16 @@
 var arrSort1 = [2, 9, 10, 6, 10];
 var arrSort2 = ["mot", "hai", "ba", "bon"];
 function labSort(arr1, callback) {
+    if (!callback) {
+        callback = function (a, b) {
+            if (a > b) {
+                return 1;
+            }
+            else {
+                return -1;
+            }
+        };
+    }
     for (var i = 0; i < arr1.length - 1; i++) {
         for (var j = i + 1; j < arr1.length; i++) {
             var newItem = callback(arr1[i], arr1[j]);
@@ -11,7 +21,8 @@ function labSort(arr1, callback) {
             }
         }
     }
+    return arr1;
 }
-var taingu = labSort(arrSort1, function (a, b) {
-    return a - b;
+var taingu = labSort(arrSort2, function (a, b) {
+    return a.localeCompare(b);
 });
--- sort.ts
+++ sort.ts
@@ -1,6 +1,15 @@
 const arrSort1 = [2,9,10,6,10];
 const arrSort2 = ["mot","hai","ba","bon"];
 function labSort<T> (arr1:T[],callback?:(a:T,b:T)=>number){
+    if(!callback){
+        callback = (a:T,b:T):number=>{
+            if(a>b){
+                return 1
+            }else{
+                return -1
+            }
+        }
+}
 for(let i = 0;i<arr1.length-1;i++){
 for(let j = i+1;j<arr1.length;i++){
     const newItem = callback(arr1[i],arr1[j]);
@@ -10,10 +19,11 @@ for(let j = i+1;j<arr1.length;i++){
         arr1[j] = temp
     }
 }
-    } 
+    }
+    return arr1 
 }
-const taingu = labSort(arrSort1,(a,b)=>{
-       return a-b;
+const taingu = labSort(arrSort2,(a,b)=>{
+       return a.localeCompare(b);
 })
 
 
