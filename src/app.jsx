dcceb08371d7f6835dbc5a088ddf9c54f96b6f2f
dinhqtai <dinhquoctai2003@gmail.com>
2023-03-25 22:08:48 +0700

    main

---
+++ src/app.jsx
@@ -0,0 +1,15 @@
+
+// component Base
+import Board from "./components/broad"
+// Props: Là một đối tượng mà dùng để truyền thông tin từ Component cha xuống component con
+// State: Là trạng thái (bộ nhớ) của component, nó quyết định componetn được hiện thị như thế nào
+const App = () => {
+
+    return <div
+        className="h-[100vh] flex flex-col justify-center items-center">
+        <Board>
+        </Board>
+    </div>
+}
+
+export default App
